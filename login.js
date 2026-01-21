538030b06791d1bc5d3fd03b0bb9fdd66d83d44a
scottludtke5-a11y <scottludtke5@gmail.com>
2026-01-21 09:15:54 -0600

    Update login.js

--- login.js
+++ login.js
@@ -5,9 +5,40 @@ function print(text) {
   gameText.innerText += text + "\n";
   gameText.scrollTop = gameText.scrollHeight;
   function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-    let currentStep = 'askUsername';
+    return new Promise(resolve => setTimeout(resolve, ms));
+    let currentStep = "askUsername";
     let username = "";
     submitBtn.addEventListener("click", async() =>{
       const input = userInput.value.trim();
       userInput.value ="";
+      if (currentStep == "askUsername") {
+        if (username === "student" || username === "teacher") {
+          print("Thank you.");
+          await sleep(1000);
+          print("Please input your password:");
+          currentStep = "askPassword";
+      }else {
+          print("Incorrect username. Please refresh page and try again.")
+          currentStep = "end";
+      }
+      } 
+    } else if (currentStep === "askPassword"){
+       const password1 = input;
+      if (username === "student" && password1 === "123456"){
+        print( "You are logged in as a student.");
+        document.getElementById("main-link").style.display = "block";
+    } else(username === "teacher" && password1 === "querty"){
+        print("You are logged in as a teacher.");
+        document.getElementById("main-link").style.display = "block";
+    }else { 
+        print("Incorrect password.");
+        await sleep(1500);
+        print("Refresh page and try again.");
+    }
+    currentStep = "end";
+    }else if(currentStep === "end"){
+      print("Session finished. Refresh page to try again");
+    }
+  });
+  print("Log-in Page \n\nWelcome! Please enter your username:")
+ if
