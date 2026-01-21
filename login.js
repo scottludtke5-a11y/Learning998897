e7262f233b6f30fb6cba7d790d092b0f5d8e56c1
scottludtke5-a11y <scottludtke5@gmail.com>
2026-01-21 09:36:58 -0600

    Update login.js

--- login.js
+++ login.js
@@ -4,30 +4,31 @@ const submitBtn = document.getElementById('submit-btn2');
 function print(text) {
   gameText.innerText += text + "\n";
   gameText.scrollTop = gameText.scrollHeight;
+}
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
     let currentStep = "askUsername";
     let username = "";
     submitBtn.addEventListener("click", async() =>{
       const input = userInput.value.trim();
       userInput.value ="";
-      if (currentStep == "askUsername") {
+      if (currentStep === "askUsername") {
+        username = input
         if (username === "student" || username === "teacher") {
           print("Thank you.");
           await sleep(1000);
           print("Please input your password:");
           currentStep = "askPassword";
-      }else {
+    } else {
           print("Incorrect username. Please refresh page and try again.")
           currentStep = "end";
-      }
-      } 
     } else if (currentStep === "askPassword"){
        const password1 = input;
       if (username === "student" && password1 === "123456"){
         print( "You are logged in as a student.");
         document.getElementById("main-link").style.display = "block";
-    } else(username === "teacher" && password1 === "querty"){
+    } else if (username === "teacher" && password1 === "qwerty"){
         print("You are logged in as a teacher.");
         document.getElementById("main-link").style.display = "block";
     }else { 
